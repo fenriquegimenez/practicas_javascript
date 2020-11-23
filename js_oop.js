a0beb83cd530091804b4c78de1cae5c075b80643
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-11-22 23:41:48 -0300

    JS OOP exercises

---
+++ js_oop.js
@@ -0,0 +1,19 @@
+class Person {
+  constructor(name, last_name, age) {
+    this.name = name;
+    this.last_name = last_name;
+    this.age = age;
+  }
+  info() {
+    return `Hola, soy ${this.name} ${this.last_name}, y tengo ${this.age} años.`;
+  }
+}
+
+student = new Person("Enrique", "Gimenez", 23);
+teacher = new Person("Sacha", "Lifszyc", 27);
+
+participants = [student, teacher];
+
+for (participant of participants) {
+  console.log(participant.info());
+}
