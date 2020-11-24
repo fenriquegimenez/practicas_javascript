562fa88729f36cf15557c432ec6114500becd20b
Enrique Gimenez <fenriquegimenez@gmail.com>
2020-11-23 22:59:28 -0300

    funcion esMayorDeEdad agregada

--- mayor_de_edad.js
+++ mayor_de_edad.js
@@ -1,12 +1,17 @@
+var mayoriaDeEdad = prompt('Por favor indique la mayoría de edad en su país:');
+
 class Person {
   constructor(name, last_name, age) {
     this.name = name;
     this.last_name = last_name;
     this.age = age;
   }
+  esMayorDeEdad() {
+    return this.age >= mayoriaDeEdad;
+  }
   mayorDeEdad() {
     var mayor;
-    if (this.age >= 18) {
+    if (this.age >= mayoriaDeEdad) {
       mayor = `${this.name} es mayor de edad`;
     } else {
       mayor = `${this.name} es menor de edad`;
